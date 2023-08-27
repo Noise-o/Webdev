7c11a4c0722247f0e8c0c1165b4ebf4970011a03
Luís Rodrigues <Luisrodrigues999@outlook.com>
2023-08-27 15:29:34 -0300

    Completo fonrmulário dark e light

--- Claro e Escuro/script.js
+++ Claro e Escuro/script.js
@@ -1,10 +1,18 @@
 const chk = document.getElementById("chk")
+const login_form = document.getElementById('login_form') 
 
 chk.addEventListener('change', () => {
-    document.body.classList.toggle("dark")
+    
+        document.body.classList.toggle("dark");
+        container.classList.toggle('dark');
+        login_form.classList.toggle('dark');
+        
+
 })
 
-function toggleCheckbox() {
-    const checkbox = document.getElementById("checkbox");
-    checkbox.checked = !checkbox.checked;
-}
+// function toggleCheckbox() {
+//     const checkbox = document.getElementById("checkbox");
+//     checkbox.checked = !checkbox.checked;
+// }
+
+

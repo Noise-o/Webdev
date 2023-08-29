248b3d0a105b0cdea9cf8cc7ab3fd5309c1835d5
Luís Rodrigues <Luisrodrigues999@outlook.com>
2023-08-29 17:34:12 -0300

    -a

--- To do List/Backend/src/app.js
+++ To do List/Backend/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = require('./router');
 
+// Criação da aplicação 
 const app = express();
 
+// Rotas
 app.use(router)
 
 module.exports = app;
--- To do List/Backend/src/router.js
+++ To do List/Backend/src/router.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
+// Rotas vem aqui 
 router.get('/',(req, res)=>{res.status(200).send('Router ligado')})
 
 module.exports =  router;
